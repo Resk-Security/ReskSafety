bb8a1ced4f88fb8b8ba6dd4056dcea86eb701001
zengdard <61885397+zengdard@users.noreply.github.com>
2026-09-02 22:03:03 +0200

    license: replace MIT with RESK Software License v1.1, remove .deepsec from repo

--- .deepsec/deepsec.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "deepsec/config";
-
-export default defineConfig({
-  projects: [
-    { id: "Resk", root: ".." },
-    // <deepsec:projects-insert-above>
-  ],
-  defaultAgent: "pi",
-});
